bf29061429329ebb66fa35d387f14082587eb53e
Icaro <icaronunes999@gmail.com>
2025-11-16 13:48:15 -0300

    feat: Adicionando as telas do jogo

---
+++ src/main/java/com/pokearena/view/TelaTreinador.java
@@ -0,0 +1,20 @@
+package com.pokearena.view;
+import com.pokearena.model.Pokemon;
+import com.pokearena.repository.BancoDeDados;
+import javafx.animation.ScaleTransition;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.util.Duration;
+
+public class TelaTreinador {
+}
