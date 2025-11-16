c421e9d349b4fe3c030902091cd968e530e80adf
Icaro <icaronunes999@gmail.com>
2025-11-16 16:17:57 -0300

    feat: Adicionando parte da tela do principal

--- src/main/java/com/pokearena/view/MenuPrincipal.java
+++ src/main/java/com/pokearena/view/MenuPrincipal.java
@@ -2,10 +2,129 @@ package com.pokearena.view;
 
 import com.pokearena.model.Pokemon;
 import com.pokearena.repository.BancoDeDados;
+import javafx.animation.ScaleTransition;
+import javafx.application.Application;
+import javafx.geometry.Pos;
+import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.control.Button;
+import javafx.util.Duration;
+
 import java.sql.Connection;
 import java.util.List;
 
-public class MenuPrincipal {
+public class MenuPrincipal extends Application {
+
+    private static final double FatorEscala = 1.1;
+    private static final int DuracaoMS = 150;
+    private static final String btnStyle = "-fx-padding: 0; -fx-background-color: transparent;" +
+                                            "-fx-min-width: 200px; -fx-min-height: 100px;";
+    public static void animacaoHover(Button botao){
+        ScaleTransition crescer = new ScaleTransition(Duration.millis(DuracaoMS),botao);
+        crescer.setToX(FatorEscala);
+        crescer.setToY(FatorEscala);
+        ScaleTransition diminuir = new ScaleTransition(Duration.millis(DuracaoMS),botao);
+        diminuir.setToX(1.0);
+        diminuir.setToY(1.0);
+
+        botao.setOnMouseEntered(e->{
+            diminuir.stop();
+            crescer.playFromStart();
+        });
+        botao.setOnMouseExited(e->{
+            crescer.stop();
+            diminuir.playFromStart();
+        });
+    }
+
+    public static void configBtn(Button btn){
+        btn.setScaleX(1.0);
+        btn.setScaleY(1.0);
+        btn.setStyle(btnStyle);
+    }
+    public void putBtnIniciarImg(Button btn){
+        Image image = new Image(getClass().getResourceAsStream("/srcPokeArena/botoes/btnIniciar.png"));
+        ImageView iv = new ImageView(image);
+        iv.setFitWidth(500);
+        iv.setFitHeight(120);
+        iv.setPreserveRatio(true);
+        btn.setGraphic(iv);
+    }
+    public void putBtnInsiImg(Button btn){
+        Image image = new Image(getClass().getResourceAsStream("/srcPokeArena/botoes/btnInsignias.png"));
+        ImageView iv = new ImageView(image);
+        iv.setFitWidth(500);
+        iv.setFitHeight(120);
+        iv.setPreserveRatio(true);
+        btn.setGraphic(iv);
+    }
+    public void putBtnPokemonImg(Button btn){
+        Image image = new Image(getClass().getResourceAsStream("/srcPokeArena/botoes/btnPokemon.png"));
+        ImageView iv = new ImageView(image);
+        iv.setFitWidth(500);
+        iv.setFitHeight(120);
+        iv.setPreserveRatio(true);
+        btn.setGraphic(iv);
+    }
+
+    public static String backgroundStyle = "-fx-background-image: url('srcPokearena/wallpaper2.jpg'); " +
+                                           "-fx-background-size: cover; " +
+                                           "-fx-background-position: center center; " +
+                                           "-fx-background-repeat: no-repeat;";
+
+    @Override
+    public void start(Stage stage){
+        Button btnIniciarJogo = new Button("");
+        Button btnInsignias = new Button("");
+        Button btnPokemon = new Button("");
+        Image logo = new Image(getClass().getResourceAsStream("/srcPokearena/logoPokearena.png"));
+        ImageView logoView = new ImageView(logo);
+        logoView.setFitWidth(800);
+        logoView.setFitHeight(180);
+        logoView.setPreserveRatio(true);
+
+        configBtn(btnIniciarJogo);
+        configBtn(btnInsignias);
+        configBtn(btnPokemon);
+
+        animacaoHover(btnIniciarJogo);
+        animacaoHover(btnInsignias);
+        animacaoHover(btnPokemon);
+
+        putBtnIniciarImg(btnIniciarJogo);
+        putBtnInsiImg(btnInsignias);
+        putBtnPokemonImg(btnPokemon);
+
+        VBox root = new VBox();
+        root.setStyle(backgroundStyle);
+        root.getChildren().add(logoView);
+        root.getChildren().add(btnIniciarJogo);
+        root.getChildren().add(btnInsignias);
+        root.getChildren().add(btnPokemon);
+        root.setSpacing(15);
+        root.setAlignment(Pos.CENTER);
+
+        Scene scene = new Scene(root,0,0);
+        stage.setTitle("PokeArena");
+        stage.setScene(scene);
+        stage.setMaximized(true);
+        stage.show();
+
+        TelaBatalha battle1 = new TelaBatalha();
+        btnIniciarJogo.setOnAction(e->{
+            double currentWidth = stage.getWidth();
+            double currentHeight = stage.getHeight();
+            Scene battleScene = battle1.criarSceneBatalha(1, currentWidth, currentHeight);
+            stage.setScene(battleScene);
+        });
+    }
 
     public static void exibirPokemons() {
         List<Pokemon> pokemons = BancoDeDados.listarPokemons();
@@ -30,6 +149,8 @@ public class MenuPrincipal {
         } else {
             System.out.println("Falha ao conectar ao banco");
         }
+
+        launch(args);
     }
 }
 
