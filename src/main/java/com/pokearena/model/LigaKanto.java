11af76f56a50ea3f8b34d02c9cd22b4041210fbb
Ícaro Nunes <icaro.nunes@rpisolucoes.com.br>
2025-11-18 17:03:29 -0300

    feat: Colocado as informações no método da classe LigaKanto

--- src/main/java/com/pokearena/model/LigaKanto.java
+++ src/main/java/com/pokearena/model/LigaKanto.java
@@ -4,9 +4,13 @@ public class LigaKanto extends Liga {
     public LigaKanto(String regiao){
         super(regiao);
     }
-
     @Override
     public String mostrarInformacoesLiga() {
-        return "";
+        return "Liga Pokémon de Kanto\n" +
+                "A Liga Pokémon de Kanto é o objetivo final de todo Treinador.\n" +
+                "\n" +
+                "Para competir, você deve primeiro derrotar os três Líderes de Ginásio da região e conquistar as suas Insígnias de mérito:\n" +
+                "\n" + "1.Brock\n" + "2.Misty\n" + "3.Ash\n" +
+                "Ao provar sua força com todas as insígnias, o Treinador se torna o Campeão de Kanto.";
     }
 }
