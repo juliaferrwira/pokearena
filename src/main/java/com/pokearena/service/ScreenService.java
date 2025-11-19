e32ad334382b903f0eb8665a5d99c4bd2b3b8fb9
Icaro <icaronunes999@gmail.com>
2025-11-18 22:05:05 -0300

    fix: Implementando funções de animação no service de telas

--- src/main/java/com/pokearena/service/ScreenService.java
+++ src/main/java/com/pokearena/service/ScreenService.java
@@ -1,13 +1,11 @@
 package com.pokearena.service;
 
-import javafx.animation.PauseTransition;
-import javafx.animation.ScaleTransition;
-import javafx.animation.SequentialTransition;
-import javafx.animation.TranslateTransition;
+import javafx.animation.*;
 import javafx.scene.Node;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.ProgressBar;
 import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
@@ -25,6 +23,32 @@ public class ScreenService {
     public int screenHeight = screenSize.height;
     public final double offScreenDistance = ((double) screenWidth /2);
 
+    // String que guarda o css da barra de hp
+    public String cssStyles = """
+        .hp-bar-default > .track {
+            -fx-control-inner-background: #444444; /* Cor escura do fundo da barra */
+            -fx-background-radius: 5;
+        }
+        .hp-full > .bar {
+            -fx-background-color: linear-gradient(to bottom, #7FFF00, #4c9600);
+            -fx-background-radius: 5;
+            -fx-padding: 3px;
+        }
+        .hp-medium > .bar {
+            -fx-background-color: linear-gradient(to bottom, #FFD700, #ff8c00);
+            -fx-background-radius: 5;
+            -fx-padding: 3px;
+        }
+        .hp-low > .bar {
+            -fx-background-color: linear-gradient(to bottom, #FF6347, #b22222);
+            -fx-background-radius: 5;
+            -fx-padding: 3px;
+        }
+        """;
+
+    // String que funciona como se fosse o arquivo do Style.css
+    public String dataUrl = "data:text/css," + cssStyles.replaceAll("\n", "").replaceAll("\"", "'");
+
     //Insignias Service
     private static final String insiStyle = "-fx-padding: 0; -fx-background-color: transparent;";
 
@@ -34,19 +58,19 @@ public class ScreenService {
         insignia.setStyle(insiStyle);
     }
 
-    public void animacaoHover(ImageView insignia){
-        ScaleTransition crescer = new ScaleTransition(Duration.millis(DuracaoMS),insignia);
+    public void animacaoHover(ImageView iv){
+        ScaleTransition crescer = new ScaleTransition(Duration.millis(DuracaoMS),iv);
         crescer.setToX(FatorEscala);
         crescer.setToY(FatorEscala);
-        ScaleTransition diminuir = new ScaleTransition(Duration.millis(DuracaoMS),insignia);
+        ScaleTransition diminuir = new ScaleTransition(Duration.millis(DuracaoMS),iv);
         diminuir.setToX(1.0);
         diminuir.setToY(1.0);
 
-        insignia.setOnMouseEntered(e->{
+        iv.setOnMouseEntered(e->{
             diminuir.stop();
             crescer.playFromStart();
         });
-        insignia.setOnMouseExited(e->{
+        iv.setOnMouseExited(e->{
             crescer.stop();
             diminuir.playFromStart();
         });
@@ -113,6 +137,34 @@ public class ScreenService {
         stage.setMaximized(true);
         stage.setScene(scene);
     }
+    // Função para atualizar a barra de HP do pokemon
+    public void atualizarBarraHP(ProgressBar hpBar, double hpAtual, double hpMax) {
+        double progress = hpAtual / hpMax;
+        hpBar.setProgress(progress);
+        hpBar.getStyleClass().removeAll("hp-full", "hp-medium", "hp-low");
+        if (progress > 0.5) {
+            hpBar.getStyleClass().add("hp-full"); // Verde (50% - 100%)
+        } else if (progress > 0.2) {
+            hpBar.getStyleClass().add("hp-medium"); // Amarelo (20% - 50%)
+        } else {
+            hpBar.getStyleClass().add("hp-low"); // Vermelho (0% - 20%)
+        }
+    }
 
+    // Função de animação para as pokebolas
+    public SequentialTransition criarTransicaoPokebolas(Node elemento) {
+
+        FadeTransition fadeIn = new FadeTransition(Duration.seconds(1.0), elemento);
+        fadeIn.setFromValue(0.0);
+        fadeIn.setToValue(1.0);
+
+        FadeTransition fadeOut = new FadeTransition(Duration.seconds(1.0), elemento);
+        fadeOut.setFromValue(1.0);
+        fadeOut.setToValue(0.0);
+
+        SequentialTransition sequencia = new SequentialTransition(fadeOut,fadeIn);
+
+        return sequencia;
+    }
     public ScreenService(){}
 }
